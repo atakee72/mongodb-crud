2cef902992ffa1aad54aac0d817204c158e7de18
atakee72 <atakee@gmail.com>
2023-08-30 00:09:02 +0200

    Added functionality to add and delete topic buttons.

--- components/RemoveBtn.jsx
+++ components/RemoveBtn.jsx
@@ -1,9 +1,26 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import { HiOutlineTrash } from "react-icons/hi";
 
-function RemoveBtn() {
+function RemoveBtn({ id }) {
+  const router = useRouter();
+  const removeTopic = async () => {
+    const confirmed = confirm("Are you sure?");
+
+    if (confirmed) {
+      const res = await fetch(`http://localhost:3000/api/topics?id=${id}`, {
+        method: "DELETE",
+      });
+
+      if (res.ok) {
+        router.refresh();
+      }
+    }
+  };
   return (
     <div>
-      <button className="text-red-400">
+      <button onClick={removeTopic} className="text-red-400">
         <HiOutlineTrash size={24} />
       </button>
     </div>
